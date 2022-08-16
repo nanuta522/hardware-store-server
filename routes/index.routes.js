72fd11962ccd53c843c876aa2cf9b067a9b3a927
Natalia Pinzon <nanuta522@gmail.com>
2022-08-16 15:36:29 +0200

    cart routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,5 +3,6 @@ const router = require("express").Router()
 router.use('/product', require('./product.routes'))
 router.use("/user", require('./user.routes'))
 router.use("/auth", require('./auth.routes'))
+router.use("/cart", require('./cart.routes'))
 
 module.exports = router
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,7 +3,7 @@ const Product = require('../models/Product.model')
 const { isAuthenticated } = require('./../middlewares/jwt.middleware')
 
 //create new product
-router.post('/create', /* isAuthenticated, */(req, res) => {
+router.post('/create', isAuthenticated,(req, res) => {
 
     const { name, description, price, images, category, inStock } = req.body
 
@@ -43,7 +43,7 @@ router.put('/:product_id/edit', (req, res) => {
     const { name, description, price, images, category, inStock } = req.body
 
     Product
-        .findByIdAndUpdate(product_id, { name, description, price, images, category, inStock }, { new: true })
+        .findByIdAndUpdate(product_id, req.body, { new: true })
         .then(product => res.status(200).json(product))
         .catch(err => res.status(500).json({ errorMessage: err.message }))
 })
