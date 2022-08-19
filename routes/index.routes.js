e969d9c4d34e34dadc660499c8293b89e63f9942
Natalia Pinzon <nanuta522@gmail.com>
2022-08-19 11:38:13 +0200

    payment route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,9 @@
-const router = require("express").Router()
+const router = require('express').Router()
 
 router.use('/product', require('./product.routes'))
-router.use("/user", require('./user.routes'))
-router.use("/auth", require('./auth.routes'))
-router.use("/cart", require('./cart.routes'))
-router.use("/payment", require('./payment.routes'))
+router.use('/user', require('./user.routes'))
+router.use('/auth', require('./auth.routes'))
+router.use('/cart', require('./cart.routes'))
+router.use('/payment', require('./payment.routes'))
 
 module.exports = router
--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -4,5 +4,41 @@ const Cart = require('../models/Cart.model')
 const Product = require('../models/Product.model')
 
 //payment process
+router.put('/', isAuthenticated, (req, res) => {
+
+    const { _id: user_id } = req.payload
+
+    Cart
+        .findOne({ user_id })
+        .populate({
+            path: 'items',
+            populate: {
+                path: 'product'
+            }
+        })
+        .then(({ items }) => {
+
+            const purchasePromises = []
+
+            items.forEach(item => {
+
+                const product_id = item.product._id
+
+                purchasePromises.push(
+                    Product
+                        .findByIdAndUpdate(product_id, { $inc: { inStock: - item.quantity } }, { new: true }),
+                )
+            })
+            purchasePromises.push(
+                Cart
+                    .findOneAndUpdate({ user_id }, { items: [] }, { new: true })
+            )
+
+            Promise
+                .all(purchasePromises)
+                .then(response => res.status(200).json({ items, response }))
+        })
+        .catch(err => res.status(500).json({ errorMessage: err.message }))
+})
 
 module.exports = router
